8d4dc9a169606ed30e3d3cb26ffe0c280b36cf36
Amine Haddou <haddouamine2002@gmail.com>
2023-11-30 01:32:13 +0100

    Version 1 of UI

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+import csv
+import os
 import pandas as pd
 from joblib import load
 from predict_match_result import predict_result, predict_result_group_stage
+from predict_championship import championship
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -34,11 +37,44 @@ def match_predictor():
 
     return render_template('match_predictor.html', winner=winner, proba=proba)
 
-@app.route('/tournament_predictor')
-def tournament_predictor():
-    # Implement your tournament predictor logic here
-    winner = "Team X"  # Replace with the actual winner
-    return render_template('tournament_predictor.html', winner=winner)
+@app.route('/save_groups', methods=['POST'])
+def save_groups():
+    groups = {}
+    csv_file_path = './app/data/groups.csv'
+    progress_info = None
+
+    for group in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
+        teams = request.form.getlist('group{}[]'.format(group))
+        groups[group] = teams
+
+    # Save the groups to a CSV file
+    save_groups_to_csv(groups, csv_file_path)
+
+    # Start the tournament and get the progress information
+    progress_info = championship(csv_file_path)
+
+    # Pass the progress information to the template
+    return render_template('tournament_predictor.html', saved_groups=groups, progress_info=progress_info)
+
+
+def save_groups_to_csv(groups, csv_file_path):
+
+    # Check if the directory exists, and create it if not
+    directory = os.path.dirname(csv_file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        # Write the header
+        csv_writer.writerow(['Group A', 'Group B', 'Group C', 'Group D', 'Group E', 'Group F', 'Group G', 'Group H'])
+
+        # Write the teams for each group
+        for index in range(4):
+            csv_writer.writerow([groups['A'][index], groups['B'][index], groups['C'][index], groups['D'][index],
+                                groups['E'][index], groups['F'][index], groups['G'][index], groups['H'][index]])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/predict_championship.py
@@ -0,0 +1,159 @@
+import itertools
+import sys
+import pandas as pd
+import csv
+import predict_match_result as pmr
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Function : Run a championship                                                                                        #
+# -------------------------------------------------------------------------------------------------------------------- #
+# Predict the final result of a championship of a given list of teams from a .CSV file. The championship is structured #
+# in 2 phases: the group stage and the knockout stage.                                                                 #
+# -------------------------------------------------------------------------------------------------------------------- #
+def championship(teams: str, model="rf"):
+
+    def import_teams(file_path):                                                   # Import group teams from a .CSV file
+        with open(file_path, newline='') as csvfile:
+
+            reader  = csv.reader(csvfile, delimiter=';')
+            next(reader)                                                                           # Skip the header row
+            columns = list(zip(*reader))                                                               # Rows to columns
+
+        return [list(column) for column in columns]
+
+    groups = import_teams(teams)                                                                          # Import teams
+    groups = [{country.strip(): 0 for country in group} for group in groups]              # Create a dict for each group
+
+    championship_progress = []                                                     # List to store championship progress
+
+    print(f"Groups: {groups}")
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # The group stage                                                                                                  #
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # For each group in the championship we predict the result of each match between teams of this group               #
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    print("\n---------------------------------------------------------------------------------------------------------")
+    print("Results of the group stage:")
+    print("---------------------------------------------------------------------------------------------------------\n")
+
+    for group in groups:
+
+        # Get all possible match combinations in the group
+        combinations = pd.DataFrame(list(itertools.combinations(group.keys(), 2)),
+                                    columns=["home_team", "away_team"])
+
+        for home_team, away_team in combinations.values:                              # Predict the result of each match
+
+            winner, probability_winner = pmr.predict_result_group_stage(home_team, away_team, False, model)
+
+            if winner == "Draw":                                                            # Update points in the group
+                group[home_team] += 1
+                group[away_team] += 1
+            else:
+                group[winner]    += 3
+
+            print(f"[{home_team} vs {away_team}]: {winner} wins with {probability_winner} probability")
+            championship_progress.append(f"[{home_team} vs {away_team}]: {winner} wins with {probability_winner} probability (Group stage -- {group})") # Append results to the list
+
+    print("\n---------------------------------------------------------------------------------------------------------")
+    print(f"Final group points: {groups}")
+    print("---------------------------------------------------------------------------------------------------------\n")
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # The knockout stage                                                                                               #
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # For each group in the championship we predict the result of each match between teams of this group               #
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    # Get the 2 best teams of each group
+    best_teams = [sorted(group.items(), key=lambda x: x[1], reverse=True)[:2] for group in groups]
+    print(f"Best teams, knockout : {best_teams}")
+
+    # New list order for the knockout stage
+    knockout_teams = []
+    for i in range(2):
+        for group in best_teams:
+            knockout_teams.append(group[i])
+
+    print("\n---------------------------------------------------------------------------------------------------------")
+    print(f"Knockout teams: {knockout_teams}")
+    print("---------------------------------------------------------------------------------------------------------\n")
+
+    # Until there only one team left, we predict the result of each match between 2 teams of the list in order
+    while len(knockout_teams) > 1:
+
+        print("\n-----------------------------------------------------------------------------------------------------")
+        print("New knockout phase:")
+        print("-----------------------------------------------------------------------------------------------------\n")
+
+        knockout_teams_updated = []
+
+        for i in range(0, len(knockout_teams), 2):
+
+            home_team                  = knockout_teams[i][0]
+            away_team                  = knockout_teams[i + 1][0]
+
+            winner, probability_winner = pmr.predict_result(home_team, away_team, False, model)
+
+            print(f"[{home_team} vs {away_team}]: {winner} wins with {probability_winner} probability")
+            championship_progress.append(f"[{home_team} vs {away_team}]: {winner} wins with {probability_winner} probability (Knockout stage)") # Append results to the list
+
+            knockout_teams_updated.append([winner, probability_winner])
+
+        knockout_teams = knockout_teams_updated
+
+    print("\n---------------------------------------------------------------------------------------------------------")
+    print(f"Winner of the championship: {knockout_teams[0][0]}")
+    print("---------------------------------------------------------------------------------------------------------\n")
+    championship_progress.append(f"Winner of the championship: {knockout_teams[0][0]}") # Append results to the list
+
+    return championship_progress
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Function : Check if chosen teams are valid                                                                           #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+def check_teams(home_team: str, away_team: str):
+    available_teams = pd.read_csv("../data/FIFA_country_list.csv",
+                                  sep=";",
+                                  names=["country"],
+                                  index_col=False
+                                  )["country"].unique()
+
+    # If country not available in list, we can't predict the result
+
+    if home_team not in available_teams or away_team not in available_teams:
+        print("There's at least one of the countries you entered that is not available.")
+        sys.exit(1)
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Function : main()                                                                                                    #
+# -------------------------------------------------------------------------------------------------------------------- #
+# Parameters:                                                                                                          #
+#   - teams: path of the .CSV file containing the list of teams                                                        #
+#   - model: type of model to use for prediction                                                                       #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+def __main__():
+    if len(sys.argv) != 3:
+
+        print("Usage: python predict_championship.py <teams_csv_path> <model_type>")
+        print("<model_type> can be 'rf' for Random Forest or 'mlpc' for MLP Classifier")
+        sys.exit(1)
+
+        # Example: python predict_championship.py ../data/championship.csv rf
+
+    championship(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    __main__()
+
+
+
+
+
+
+
